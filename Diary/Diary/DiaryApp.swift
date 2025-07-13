6ca4e73a637bcac3663be6e0b7307b5d17d51469
김진혁 <wlsgurrla716@naver.com>
2025-07-14 01:32:34 +0900

    🔧[Chore] - 프로젝트 초기설정

---
+++ Diary/Diary/DiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryApp.swift
+//  Diary
+//
+//  Created by jinhyeokKim on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct DiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
