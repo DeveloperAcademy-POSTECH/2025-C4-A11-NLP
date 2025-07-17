60e823f7be5db87c625496624b73eb49eaa03b50
김진혁 <wlsgurrla716@naver.com>
2025-07-17 16:09:40 +0900

    🔧 [Chore] - SwiftData 모델 추가

--- Diary/Diary/Data/SwiftData.swift
+++ Diary/Diary/Data/SwiftData.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import SwiftData
+
+// MARK: - 다이어리 SwiftData 모델
+@Model
+class ChallengeModel {
+    
+    var createDate: String // ID
+    var diaryContent: String
+    var wiseSaying: String
+    var retrospective: String
+    var resolution: String
+    var summary: String
+    
+    
+    init(createDate: String, diaryContent: String, wiseSaying: String, retrospective: String, resolution: String, summary: String) {
+        self.createDate = createDate
+        self.diaryContent = diaryContent
+        self.wiseSaying = wiseSaying
+        self.retrospective = retrospective
+        self.resolution = resolution
+        self.summary = summary
+    }
+    
+}
