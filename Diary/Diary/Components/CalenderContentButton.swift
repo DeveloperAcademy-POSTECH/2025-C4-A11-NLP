c4ec961bade232737c9fa16ff709b68f2302dd06
김진혁 <wlsgurrla716@naver.com>
2025-07-23 20:02:45 +0900

    🔧 [Chore] #6 - ColorAssets 추가

---
+++ Diary/Diary/Components/CalenderContentButton.swift
@@ -0,0 +1,18 @@
+//
+//  CalenderContentButton.swift
+//  Diary
+//
+//  Created by jinhyeokKim on 7/23/25.
+//
+
+import SwiftUI
+
+struct CalenderContentButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalenderContentButton()
+}
