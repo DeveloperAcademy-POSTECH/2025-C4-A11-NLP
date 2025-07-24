24c114bfbb20b20b2e13136b2d227856b5e9692c
김진혁 <wlsgurrla716@naver.com>
2025-07-24 13:50:11 +0900

    ✨[Feat] #6 이전, 다음 컴포넌트 제작

--- Diary/Diary/Components/CalenderContentButton.swift
+++ Diary/Diary/Components/CalenderContentButton.swift
@@ -8,11 +8,49 @@
 import SwiftUI
 
 struct CalenderContentButton: View {
+    
+    let title: String
+    let imageType: CalenderContentButtonType
+    let action: () -> ()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Button {
+            action()
+        } label: {
+            RoundedRectangle(cornerRadius: 15)
+                .fill(Color.blue3)
+                .overlay {
+                    switch imageType {
+                    case .previous:
+                        HStack(spacing: 8) {
+                            Group {
+                                Image(systemName: imageType.image)
+                                Text(title)
+                            }
+                            .foregroundStyle(Color.black)
+                            .font(Font.caption1Emphasized)
+                        }
+                    case .next:
+                        HStack(spacing: 8) {
+                            Group {
+                                Text(title)
+                                Image(systemName: imageType.image)
+                            }
+                            .foregroundStyle(Color.black)
+                            .font(Font.caption1Emphasized)
+                        }
+                    }
+                }
+        }
     }
 }
 
 #Preview {
-    CalenderContentButton()
+    CalenderContentButton(title: "이전", imageType: .previous) {
+        print("이전")
+    }
+    CalenderContentButton(title: "다음", imageType: .next) {
+        print("다음")
+    }
 }
