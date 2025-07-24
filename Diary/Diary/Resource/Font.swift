e237bbb956b738e0dd2c1c02ceabf0f9f4e13da0
김진혁 <wlsgurrla716@naver.com>
2025-07-24 15:38:25 +0900

    🔧[Chore] #6 폰트 수정, 추가

--- Diary/Diary/Resource/Font.swift
+++ Diary/Diary/Resource/Font.swift
@@ -15,10 +15,16 @@ extension Font {
     static var title1Emphasized: Font {
         .system(size: 20, weight: .bold)
     }
+    static var title2: Font {
+        .system(size: 17, weight: .medium)
+    }
     static var body1: Font {
         .system(size: 22, weight: .regular)
     }
     static var body2Regular: Font {
+        .system(size: 17, weight: .regular)
+    }
+    static var body2Emphasized: Font {
         .system(size: 17, weight: .semibold)
     }
     static var caption1Emphasized: Font {
