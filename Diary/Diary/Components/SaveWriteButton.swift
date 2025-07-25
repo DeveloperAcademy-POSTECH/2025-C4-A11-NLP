263c640309d291b7cf959ffeba4ad39377b1c3f7
김진혁 <wlsgurrla716@naver.com>
2025-07-26 04:11:40 +0900

    ✨[Feat] #18 - 저장,회고 컴포넌트 구현

---
+++ Diary/Diary/Components/SaveWriteButton.swift
@@ -0,0 +1,40 @@
+//
+//  SaveWriteButton.swift
+//  Diary
+//
+//  Created by jinhyeokKim on 7/26/25.
+//
+
+import SwiftUI
+
+struct SaveWriteButton: View {
+    let title: String
+    let action: () -> ()
+    
+    var body: some View {
+        
+        Button {
+            action()
+        } label: {
+            RoundedRectangle(cornerRadius: 15)
+                .fill(Color.blue1)
+                .overlay {
+                    Text(title)
+                        .font(Font.caption1Emphasized)
+                        .foregroundStyle(Color.white)
+                }
+        }
+    }
+}
+
+#Preview() {
+    SaveWriteButton(title: "저장하기") {
+        print("저장하기")
+    }
+    .frame(width: 100, height: 44)
+    
+    SaveWriteButton(title: "회고하기") {
+        print("회고하기")
+    }
+    .frame(width: 100, height: 44)
+}
