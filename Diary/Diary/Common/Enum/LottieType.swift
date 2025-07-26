8226333a245f9fed43a44fbcca73564e7cd0e709
김진혁 <wlsgurrla716@naver.com>
2025-07-26 14:42:06 +0900

    ✨[Feat] #18 - LottieView 구현

---
+++ Diary/Diary/Common/Enum/LottieType.swift
@@ -0,0 +1,23 @@
+//
+//  LottieType.swift
+//  Diary
+//
+//  Created by jinhyeokKim on 7/26/25.
+//
+
+import Foundation
+
+enum LottieType {
+    case confettie
+    case fire
+    case loading
+    
+    var lottie: String {
+        switch self {
+        case .confettie: return "confettie"
+        case .fire: return "fire"
+        case .loading: return "loading"
+            
+        }
+    }
+}
---
+++ Diary/Diary/Presentations/Diary/Diary/Views/LottieView.swift
@@ -0,0 +1,30 @@
+//
+//  LottieView.swift
+//  Diary
+//
+//  Created by jinhyeokKim on 7/26/25.
+//
+
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+    let name: String          // Lottie 파일 이름
+    var loopMode: LottieLoopMode = .loop
+
+    func makeUIView(context: Context) -> LottieAnimationView {
+        let animationView = LottieAnimationView(name: name)
+        animationView.loopMode = loopMode
+        animationView.play()
+        return animationView
+    }
+
+    func updateUIView(_ uiView: LottieAnimationView, context: Context) {
+        
+    }
+}
+
+#Preview {
+    LottieView(name: "fire")
+        .frame(width: 200, height: 200)
+}
