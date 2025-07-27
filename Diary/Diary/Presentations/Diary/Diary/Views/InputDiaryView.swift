602b2627630be08629bbd11af1df2496414d928a
김진혁 <wlsgurrla716@naver.com>
2025-07-27 22:25:47 +0900

    ✨[Feat] #18 - FoudnationMoels 로딩시, Lottie 로딩화면 추가

--- Diary/Diary/Presentations/Diary/Diary/Views/InputDiaryView.swift
+++ Diary/Diary/Presentations/Diary/Diary/Views/InputDiaryView.swift
@@ -20,76 +20,105 @@ struct InputDiaryView: View {
     @Environment(\.diaryVM) private var diaryVM
     
     @State private var testLabel: String = ""
-    
+    @State private var isLoading: Bool = false   // 추가
+
     let date: Date
     
     let analyzer: SentimentViewModel = .init()
     
     var body: some View {
-        VStack(spacing: 0) {
-            topProgressBarAndNavigationTitleView
-                .padding(.horizontal, 16)
-            
-            ScrollView {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("오늘 하루는 어땠나요?")
-                        .font(.system(size: 17, weight: .semibold))
-                        .padding(.bottom, 8)
-                    Text("\(date.formattedWithWeekday)")
-                        .font(.caption)
-                        .foregroundStyle(Color.gray)
-                        .padding(.bottom, 27)
-                    TextField("글을 작성해주세요.", text: $diaryText, axis: .vertical)
-                        .font(.body)
-                        .focused($isTextFieldFocused)
-                    
-                    if showRedFeedback {
-                        Text("*200자 이상 글을 작성해주세요.")
-                            .font(.caption1Emphasized)
-                            .foregroundStyle(Color.red)
-                    }
-                    //mlmodel 테스트
-//                    ForEach(results, id: \.0) { label, confidence in
-//                        Text("• \(label): \(confidence, specifier: "%.1f")%")
-//                    }
+        
+        if isLoading {
+            ZStack(alignment: .center) {
+                VStack() {
+                    LottieView(name: "loading")
+                        .frame(width: 76, height: 60)
+                        Text("오늘 당신이 쓴 일기를 읽고 있어요.\n도움이 될 만한 명언들을 추천해드릴게요.")
+                            .font(Font.body2Regular)
+                            .foregroundStyle(Color.black)
+                            .multilineTextAlignment(.center)
+                            .lineSpacing(8)
+                            .padding(.top, 16)
                 }
-                .padding(.horizontal)
-                .padding(.top, 16)
-                .frame(maxWidth: .infinity, alignment: .leading)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
             }
-            .overlay(alignment: .bottom) {
-                Button {
-                    results = analyzer.predictTopSentiments(for: diaryText, count: 3)
-                    diaryVM.diary.createDate = self.date
-                    diaryVM.diary.diaryContent = diaryText // 다이어리 컨텐츠 뷰모델 저장
-                    if diaryText.count > 25 {
-                        Task {
-                            diaryVM.diary.summary = try await diaryVM.summarize(diaryText)
-                        }
-                    } else {
-                        diaryVM.diary.summary = diaryText
-                    }
-                    router.push(to: .wiseSayingView)
-                } label: {
-                    HStack(spacing: 4) {
-                        Text("오늘의 명언 받기")
-                    }
-                    .font(.caption)
-                    .foregroundStyle(.white)
-                    .frame(height: 44)
-                    .padding(.horizontal, 16)
-                    .background(
-                         Color.blue
-                    )
-                    .cornerRadius(15)
-                    .offset(x: shakeOffset)
-                }
-                Spacer()
-            }
-            .padding(.bottom, 32)
         }
+        else {
+            VStack(spacing: 0) {
+                topProgressBarAndNavigationTitleView
+                    .padding(.horizontal, 16)
+                
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text("오늘 하루는 어땠나요?")
+                            .font(.system(size: 17, weight: .semibold))
+                            .padding(.bottom, 8)
+                        Text("\(date.formattedWithWeekday)")
+                            .font(.caption)
+                            .foregroundStyle(Color.gray)
+                            .padding(.bottom, 27)
+                        TextField("글을 작성해주세요.", text: $diaryText, axis: .vertical)
+                            .font(.body)
+                            .focused($isTextFieldFocused)
+                        
+                        if showRedFeedback {
+                            Text("*200자 이상 글을 작성해주세요.")
+                                .font(.caption1Emphasized)
+                                .foregroundStyle(Color.red)
+                        }
+                        //mlmodel 테스트
+                        //                    ForEach(results, id: \.0) { label, confidence in
+                        //                        Text("• \(label): \(confidence, specifier: "%.1f")%")
+                        //                    }
+                    }
+                    .padding(.horizontal)
+                    .padding(.top, 16)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                .overlay(alignment: .bottom) {
+                    Button {
+                        results = analyzer.predictTopSentiments(for: diaryText, count: 3)
+                        diaryVM.diary.createDate = self.date
+                        diaryVM.diary.diaryContent = diaryText // 다이어리 컨텐츠 뷰모델 저장
+                        
+                        // 프로그래스바 시작
+                        isLoading = true
+                        
+                        Task {
+                            if diaryText.count > 25 {
+                                diaryVM.diary.summary = try await diaryVM.summarize(diaryText)
+                            } else {
+                                diaryVM.diary.summary = diaryText
+                            }
+                            // 요약 끝, 로딩 끝, 다음 화면으로!
+                            await MainActor.run {
+                                isLoading = false
+                                router.push(to: .wiseSayingView)
+                            }
+                        }
+                    } label: {
+                        HStack(spacing: 4) {
+                            Text("오늘의 명언 받기")
+                        }
+                        .font(.caption)
+                        .foregroundStyle(.white)
+                        .frame(height: 44)
+                        .padding(.horizontal, 16)
+                        .background(
+                            Color.blue
+                        )
+                        .cornerRadius(15)
+                        .offset(x: shakeOffset)
+                    }
+                    Spacer()
+                }
+                .padding(.bottom, 32)
+                
+                
+            }
             .background(Color(.systemGray6))
             .ignoresSafeArea(edges: .bottom)
+        }
         
     }
     
