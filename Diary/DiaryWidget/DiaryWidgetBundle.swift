d9c6892ec4bf735769c893c4bb80582889fc868c
Jijoo Maeng <mjiju06@gmail.com>
2025-07-28 14:54:48 +0900

    ✨[Feat] #24 - 위젯 extension 추가 및 기본 세팅
    # Conflicts:
    #	Diary/Diary.xcodeproj/project.pbxproj

---
+++ Diary/DiaryWidget/DiaryWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryWidgetBundle.swift
+//  DiaryWidget
+//
+//  Created by JiJooMaeng on 7/28/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DiaryWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DiaryWidget()
+//        DiaryWidgetControl()
+    }
+}
